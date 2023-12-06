27804e1317f58d806fb79d8283bded58173b7151
Quentin <xiaoxiao921@hotmail.fr>
2023-12-06 16:42:43 +0100

    feat(lua): log is now much more lua like. also make lua `print` and `error` work.

--- src/lua/bindings/log.cpp
+++ src/lua/bindings/log.cpp
@@ -1,9 +1,24 @@
 #pragma once
 #include "log.hpp"
+
 #include "lua/lua_module.hpp"
 
 namespace lua::log
 {
+	static sol::protected_function tostring;
+
+	static void log_internal(sol::variadic_args& args, sol::this_environment& env, al::eLogLevel level)
+	{
+		std::stringstream data;
+
+		for (const auto& arg : args)
+		{
+			data << tostring(arg).get<const char*>() << '\t';
+		}
+
+		LOG(level) << big::lua_module::guid_from(env) << ": " << data.str();
+	}
+
 	// Lua API: Table
 	// Name: log
 	// Table containing functions for printing to console / log file.
@@ -11,39 +26,54 @@ namespace lua::log
 	// Lua API: Function
 	// Table: log
 	// Name: info
-	// Param: data: string
+	// Param: args: any
 	// Logs an informational message.
-	static void info(const std::string& data, sol::this_environment env)
+	static void info(sol::variadic_args args, sol::this_environment env)
 	{
-		LOG(INFO) << big::lua_module::guid_from(env) << ": " << data;
+		log_internal(args, env, INFO);
 	}
 
 	// Lua API: Function
 	// Table: log
 	// Name: warning
-	// Param: data: string
+	// Param: args: any
 	// Logs a warning message.
-	static void warning(const std::string& data, sol::this_environment env)
+	static void warning(sol::variadic_args args, sol::this_environment env)
 	{
-		LOG(WARNING) << big::lua_module::guid_from(env) << ": " << data;
+		log_internal(args, env, WARNING);
 	}
 
 	// Lua API: Function
 	// Table: log
 	// Name: debug
-	// Param: data: string
+	// Param: args: any
 	// Logs a debug message.
-	static void debug(const std::string& data, sol::this_environment state)
+	static void debug(sol::variadic_args args, sol::this_environment env)
 	{
-		sol::environment& env = state;
-		LOG(VERBOSE) << big::lua_module::guid_from(env) << ": " << data;
+		log_internal(args, env, VERBOSE);
+	}
+
+	// Lua API: Function
+	// Table: log
+	// Name: error
+	// Param: args: any
+	// Logs an error message.
+	static void error(sol::variadic_args args, sol::this_environment env)
+	{
+		log_internal(args, env, FATAL);
 	}
 
 	void bind(sol::state& state)
 	{
+		tostring = state["tostring"];
+
+		state["print"] = info;
+		state["error"] = error;
+
 		auto ns       = state["log"].get_or_create<sol::table>();
 		ns["info"]    = info;
 		ns["warning"] = warning;
 		ns["debug"]   = debug;
+		ns["error"]   = error;
 	}
 }
