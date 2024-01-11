6e81b84eade3cdb3987edfedf1063eb3f9452668
xiaoxiao921 <xiaoxiao921@hotmail.fr>
2024-01-11 18:36:42 +0100

    feat(lua): expose config folder

--- src/lua/lua_module.cpp
+++ src/lua/lua_module.cpp
@@ -14,9 +14,11 @@ namespace big
 		// Each mod/plugin have their own global table containing helpers, such as:
 		// - Their own guid
 		// 
-		// - Path to their own folder inside `plugins_data`
+		// - Path to their own folder inside `config`: Used for data that must persist between sessions that can be manipulated by the user.
 		// 
-		// - Path to their own folder inside `plugins`
+		// - Path to their own folder inside `plugins_data`: Used for data that must persist between sessions but not be manipulated by the user.
+		// 
+		// - Path to their own folder inside `plugins`: Location of .lua, README, manifest.json files.
 		// 
 		// You can access other mods helpers through the `mods[OTHER_MOD_GUID]` table.
 		// 
@@ -35,6 +37,14 @@ namespace big
 		// Field: !guid: string
 		// Guid of the mod.
 		m_env["!guid"] = m_info.m_guid;
+
+		// Lua API: Field
+		// Table: _ENV - Plugin Specific Global Table
+		// Field: !config_mod_folder_path: string
+		// Path to the mod folder inside `config`
+		auto config_mod_folder_path = g_file_manager.get_project_folder("config").get_path() / m_info.m_guid;
+		auto config_mod_folder_path_string = std::string(reinterpret_cast<const char*>(config_mod_folder_path.u8string().c_str()));
+		m_env["!config_mod_folder_path"] = config_mod_folder_path_string;
 
 		// Lua API: Field
 		// Table: _ENV - Plugin Specific Global Table
