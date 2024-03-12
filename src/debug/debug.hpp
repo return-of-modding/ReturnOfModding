1c5dcfcd974fca975af7675429054d3dec7427bc
xiaoxiao921 <xiaoxiao921@hotmail.fr>
2024-03-12 16:57:08 +0100

    fix(debugging): don't wait for debugger in release builds.

--- src/debug/debug.hpp
+++ src/debug/debug.hpp
@@ -4,9 +4,14 @@ namespace big::debug
 {
 	inline void wait_until_debugger()
 	{
+#ifndef OPTIMIZE
 		while (!IsDebuggerPresent())
 		{
 			std::this_thread::sleep_for(1s);
 		}
+#else
+		LOG(VERBOSE) << "Not waiting for debugger because it's a release build.";
+		Logger::FlushQueue();
+#endif
 	}
 } // namespace big::debug
