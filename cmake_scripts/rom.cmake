d1d85a0dd2177c5b80632fe4f2b14d964f1f38c5
xiaoxiao921 <xiaoxiao921@hotmail.fr>
2024-05-04 11:26:57 +0200

    bump rombase

--- cmake_scripts/rom.cmake
+++ cmake_scripts/rom.cmake
@@ -5,6 +5,6 @@ set(LUA_GIT_HASH 5d708c3f9cae12820e415d4f89c9eacbe2ab964b)
 FetchContent_Declare(
 	rom
 	GIT_REPOSITORY https://github.com/xiaoxiao921/ReturnOfModdingBase.git
-	GIT_TAG f3bd826061883331e6f577a62a04bfd446519c26
+	GIT_TAG 1667090b021200caa85d201e6a6236a64c1f9df1
 )
 FetchContent_MakeAvailable(rom)
