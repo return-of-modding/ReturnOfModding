283fe604f729e63e9f9cd20f7c996b7ec6bcf6f3
xiaoxiao921 <837334+xiaoxiao921@users.noreply.github.com>
2024-05-27 02:02:30 +0200

    file_watcher: fixes

--- cmake_scripts/rom.cmake
+++ cmake_scripts/rom.cmake
@@ -5,6 +5,6 @@ set(LUA_GIT_HASH 5d708c3f9cae12820e415d4f89c9eacbe2ab964b)
 FetchContent_Declare(
 	rom
 	GIT_REPOSITORY https://github.com/xiaoxiao921/ReturnOfModdingBase.git
-	GIT_TAG 6d4fbb379d03a40bbbbf7244293f975d0c72de0d
+	GIT_TAG 2b7f933f9a186545b5accabd3d5350abec14a1b1
 )
 FetchContent_MakeAvailable(rom)
