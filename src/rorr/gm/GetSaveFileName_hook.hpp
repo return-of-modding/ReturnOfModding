92a3bdd459c561f7a6c2d34396c3777c906564ab
xiaoxiao921 <837334+xiaoxiao921@users.noreply.github.com>
2024-09-04 21:52:03 +0200

    fix(path): tentative fix for file system sandbox bypass not working under wine

--- src/rorr/gm/GetSaveFileName_hook.hpp
+++ src/rorr/gm/GetSaveFileName_hook.hpp
@@ -5,14 +5,25 @@ namespace gm
 {
 	inline bool hook_GetSaveFileName(char* output_path, __int64 size_of_output_path, const char* input_path)
 	{
-		const auto file_path = std::filesystem::path(input_path);
-
-		// Fix ReturnOfModding paths getting fucked by the game sandbox.
-		if (file_path.is_absolute() && file_path.native().contains(L"ReturnOfModding"))
+		try
 		{
-			strcpy(output_path, input_path);
+			const auto file_path = std::filesystem::path(input_path);
 
-			return 0;
+			// Fix ReturnOfModding paths getting fucked by the game sandbox.
+			if (strstr((char*)file_path.u8string().c_str(), "ReturnOfModding"))
+			{
+				strcpy(output_path, input_path);
+
+				return 0;
+			}
+		}
+		catch (const std::exception& e)
+		{
+			LOG(ERROR) << e.what();
+		}
+		catch (...)
+		{
+			LOG(ERROR) << "Unknown exception";
 		}
 
 		const auto res = big::g_hooking->get_original<hook_GetSaveFileName>()(output_path, size_of_output_path, input_path);
