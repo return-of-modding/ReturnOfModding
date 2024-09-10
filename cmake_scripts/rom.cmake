0660d3660cb53b88835a2d2138ff6fd977153f60
xiaoxiao921 <837334+xiaoxiao921@users.noreply.github.com>
2024-09-10 22:39:53 +0200

    rombase: update to latest, check upstream for changelog

--- cmake_scripts/rom.cmake
+++ cmake_scripts/rom.cmake
@@ -5,6 +5,6 @@ set(LUA_GIT_HASH 5d708c3f9cae12820e415d4f89c9eacbe2ab964b)
 FetchContent_Declare(
 	rom
 	GIT_REPOSITORY https://github.com/xiaoxiao921/ReturnOfModdingBase.git
-	GIT_TAG 2822847f543cfdc094ef01af90aadd59985867b5
+	GIT_TAG bc6fda87c436dbb39df8a1a4b3a4b7dda894ee8d
 )
 FetchContent_MakeAvailable(rom)
