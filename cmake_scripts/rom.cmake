5afdd8c22b83edc3eca2f30990df4de4318e0365
xiaoxiao921 <837334+xiaoxiao921@users.noreply.github.com>
2024-09-10 15:43:31 +0200

    rombase: update to latest (max instance too small)

--- cmake_scripts/rom.cmake
+++ cmake_scripts/rom.cmake
@@ -5,6 +5,6 @@ set(LUA_GIT_HASH 5d708c3f9cae12820e415d4f89c9eacbe2ab964b)
 FetchContent_Declare(
 	rom
 	GIT_REPOSITORY https://github.com/xiaoxiao921/ReturnOfModdingBase.git
-	GIT_TAG 12dbb29adb41db6df3e6c5a2a387b0280c158093
+	GIT_TAG 2822847f543cfdc094ef01af90aadd59985867b5
 )
 FetchContent_MakeAvailable(rom)
