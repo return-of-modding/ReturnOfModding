91f2bc20e8f721e4a3d06efe36e0fddab4e413c0
xiaoxiao921 <837334+xiaoxiao921@users.noreply.github.com>
2024-10-18 19:24:38 +0200

    bump rombase (fix unicode cmd line arg parsing)

--- cmake_scripts/rom.cmake
+++ cmake_scripts/rom.cmake
@@ -5,6 +5,6 @@ set(LUA_GIT_HASH 5d708c3f9cae12820e415d4f89c9eacbe2ab964b)
 FetchContent_Declare(
 	rom
 	GIT_REPOSITORY https://github.com/xiaoxiao921/ReturnOfModdingBase.git
-	GIT_TAG 1ce1fb1b996fa04f4d805a2b0c782cd554e43e57
+	GIT_TAG c44a49e1c34327a546e48cd81072f7665dc37488
 )
 FetchContent_MakeAvailable(rom)
