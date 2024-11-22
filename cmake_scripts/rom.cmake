9309b0b56be3af7768b7189dee1698dc806d61b5
xiaoxiao921 <837334+xiaoxiao921@users.noreply.github.com>
2024-11-22 01:44:36 +0100

    fix rombase main.cpp file and lib/exe type not in test mode

--- cmake_scripts/rom.cmake
+++ cmake_scripts/rom.cmake
@@ -5,6 +5,6 @@ set(LUA_GIT_HASH 5d708c3f9cae12820e415d4f89c9eacbe2ab964b)
 FetchContent_Declare(
 	rom
 	GIT_REPOSITORY https://github.com/xiaoxiao921/ReturnOfModdingBase.git
-	GIT_TAG f62b2697c0a4a6ba3b2afa4dbd3d1a06fcc400a9
+	GIT_TAG 2033f0ea6ddfc62c2b036cf7f8cd4500065f5c9f
 )
 FetchContent_MakeAvailable(rom)
