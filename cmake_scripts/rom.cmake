19a950d6887ad2ef65c6a9a86a7f29715022891b
Quentin <837334+xiaoxiao921@users.noreply.github.com>
2024-12-01 22:28:11 +0100

    bump rombase, fix for dynamic_hook_mid

--- cmake_scripts/rom.cmake
+++ cmake_scripts/rom.cmake
@@ -5,6 +5,6 @@ set(LUA_GIT_HASH 5d708c3f9cae12820e415d4f89c9eacbe2ab964b)
 FetchContent_Declare(
 	rom
 	GIT_REPOSITORY https://github.com/xiaoxiao921/ReturnOfModdingBase.git
-	GIT_TAG 2033f0ea6ddfc62c2b036cf7f8cd4500065f5c9f
+	GIT_TAG 5bbc2cc0a7060eb404798c8898d6ae172dcd04f0
 )
 FetchContent_MakeAvailable(rom)
