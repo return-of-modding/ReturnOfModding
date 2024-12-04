21df876292e8cbb54e17caede367a0c93e34d7d3
xiaoxiao921 <837334+xiaoxiao921@users.noreply.github.com>
2024-12-04 22:22:12 +0100

    bump rombase, fixing filesystem functions not following symlinks

--- cmake_scripts/rom.cmake
+++ cmake_scripts/rom.cmake
@@ -5,6 +5,6 @@ set(LUA_GIT_HASH 5d708c3f9cae12820e415d4f89c9eacbe2ab964b)
 FetchContent_Declare(
 	rom
 	GIT_REPOSITORY https://github.com/xiaoxiao921/ReturnOfModdingBase.git
-	GIT_TAG e77e5811656710c405a8b16ee32602abe2d068c7
+	GIT_TAG a45352418a69838854072b1ad5c2f3a0b91ab551
 )
 FetchContent_MakeAvailable(rom)
