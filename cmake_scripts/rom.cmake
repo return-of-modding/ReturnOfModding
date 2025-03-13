bc1b893398759a0c73b68e53322e9064b6fe6ec0
xiaoxiao921 <837334+xiaoxiao921@users.noreply.github.com>
2025-03-13 03:22:42 +0100

    more progress

--- cmake_scripts/rom.cmake
+++ cmake_scripts/rom.cmake
@@ -3,6 +3,6 @@ include(FetchContent)
 FetchContent_Declare(
 	rom
 	GIT_REPOSITORY https://github.com/xiaoxiao921/ReturnOfModdingBase.git
-	GIT_TAG 7845618614f65079657d5540f11f9300a47c7bce
+	GIT_TAG 606797adb666e36453d9a2a419244b18cdfd93e6
 )
 FetchContent_MakeAvailable(rom)
