2471f5212f17b6272e7253464e81032a1db5139f
xiaoxiao921 <837334+xiaoxiao921@users.noreply.github.com>
2025-03-31 07:39:19 +0200

    bump rombase: mods.loading_order fix

--- cmake_scripts/rom.cmake
+++ cmake_scripts/rom.cmake
@@ -6,6 +6,6 @@ set(LUA_GIT_HASH 5d708c3f9cae12820e415d4f89c9eacbe2ab964b)
 FetchContent_Declare(
 	rom
 	GIT_REPOSITORY https://github.com/xiaoxiao921/ReturnOfModdingBase.git
-	GIT_TAG 35352e9d3f9de5f7e3ccccb227551ec8f7d0a027
+	GIT_TAG e26dc7080577110c1af78b903c27a284cff10296
 )
 FetchContent_MakeAvailable(rom)
