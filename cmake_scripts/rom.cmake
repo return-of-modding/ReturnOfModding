466e8e45f8369f81b469c21f78f857e9d8fa2ec8
xiaoxiao921 <837334+xiaoxiao921@users.noreply.github.com>
2025-05-30 21:54:05 +0200

    bump rombase (better exception handling)

--- cmake_scripts/rom.cmake
+++ cmake_scripts/rom.cmake
@@ -3,6 +3,6 @@ include(FetchContent)
 FetchContent_Declare(
 	rom
 	GIT_REPOSITORY https://github.com/xiaoxiao921/ReturnOfModdingBase.git
-	GIT_TAG 8725e54ba39dca0236107bdeada76294bd4e957b
+	GIT_TAG 2baafbac6f99d48432a17aec82add599cf748365
 )
 FetchContent_MakeAvailable(rom)
