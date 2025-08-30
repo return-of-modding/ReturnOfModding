3640ea3f11c8615beb3dbe69e7106b5a75f16bf5
Quentin <837334+xiaoxiao921@users.noreply.github.com>
2025-08-30 19:11:50 +0200

    fix(imgui): fix asserts not being disabled in debug mode

--- cmake_scripts/rom.cmake
+++ cmake_scripts/rom.cmake
@@ -3,6 +3,10 @@ include(FetchContent)
 set(LUA_USE_LUAJIT false)
 set(LUA_GIT_HASH 5d708c3f9cae12820e415d4f89c9eacbe2ab964b) # https://github.com/lua/lua/releases/tag/v5.4.4
 
+add_compile_definitions(
+    "IMGUI_USER_CONFIG=\"${SRC_DIR}/gui/imgui_config.hpp\""
+)
+
 FetchContent_Declare(
 	rom
 	GIT_REPOSITORY https://github.com/xiaoxiao921/ReturnOfModdingBase.git
